95bb4935829102f3af12d67d08726a1fc4addf10
stemma1704 <137061557+stemma1704@users.noreply.github.com>
2023-07-02 20:15:06 +0530

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -185,7 +185,7 @@ def page1():
                         """,
                 unsafe_allow_html=True,
             )
-            st.markdown('<p class="big-font">Life is too short for ordinary apps!</p>', unsafe_allow_html=True)
+            st.markdown('<p class="big-font">Beyond the Silver Screen: Unraveling Movie Revenue Mysteries!</p>', unsafe_allow_html=True)
             st.markdown(
                 """
                         <style>
@@ -195,7 +195,7 @@ def page1():
                         """,
                 unsafe_allow_html=True,
             )
-            st.markdown('<p class="big-font2" >#FastForwardYourFuture</p>', unsafe_allow_html=True)
+            st.markdown('<p class="big-font2" >#DesignedByStemyTomy</p>', unsafe_allow_html=True)
 
 
         st.markdown("""<hr style="height:10px;border:none;color:#333;background-color:#333;" /> """, unsafe_allow_html=True)
